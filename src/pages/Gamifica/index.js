505ae871d29dc6bf94c739c05795414d968d83da
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 20:36:51 -0300

    buscar dados de pontuação do backend

--- src/pages/Gamifica/index.js
+++ src/pages/Gamifica/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect , useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -30,17 +30,47 @@ import Tab from '@material-ui/core/Tab';
 
 import "./gami.css"
 
+import api from '../../services/api';
+
 function Copyright() {
   return (
+
     <Typography variant="body2" color="textSecondary" align="center">
+      
       {'Copyright © '}
+      
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
       </Link>{' '}
+      
       {new Date().getFullYear()}
       {'.'}
+    
     </Typography>
   );
+}
+
+function Gamification() {
+
+  const clienteId = "5fb05d60a3008ba9c6e59137";
+
+  const [quantidadePontosAtual, setQuantidadePontosAtual] = useState([]);
+  const [quantidadeXP, setQuantidadeXP] = useState([]);
+  const [QuantidadePontosXpNecessariosParaAtingirProximoNivel, setQuantidadePontosXpNecessariosParaAtingirProximoNivel] = useState([]);
+
+  useEffect(() => {
+
+    async function loadScores() {
+
+      const response = await api.get(`/pontuacaoCliente/${clienteId}`);
+
+      setQuantidadePontosAtual(response.data.QuantidadePontosAtual);
+      setQuantidadeXP(response.data.QuantidadeXP);
+      setQuantidadePontosXpNecessariosParaAtingirProximoNivel(response.data.QuantidadePontosXpNecessariosParaAtingirProximoNivel);
+    }
+
+    loadScores();
+  }, []);
 }
 
 const drawerWidth = 240;
@@ -125,14 +155,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard() {
+  
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
+  
   const handleDrawerClose = () => {
     setOpen(false);
   };
+  
+  Gamification();
+
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
-
 import axios from "axios";
+
 const api = axios.create({
-    baseURL: "https://localhost/",
-})
+    baseURL: "https://desafiogamificacaocpfl20201114183303.azurewebsites.net/",
+});
+
 export default api;
