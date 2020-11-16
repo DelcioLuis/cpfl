3902509b25e21453dad0c46b30efe032cb42d229
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 21:16:02 -0300

    feature: Adicionado resgate de pontos

---
+++ src/pages/Gamifica/ResgatePontos.js
@@ -0,0 +1,30 @@
+import { useEffect , useState } from 'react';
+import api from '../../services/api';
+
+export default function ResgatePontos() {
+
+  const idCliente = "5fb05d60a3008ba9c6e59137"; // Delcio
+
+  const [pontosResgatados, setPontosResgatados] = useState(5);
+
+  let totalPontosCliente = 0;
+  let mensagem = "";
+
+  useEffect(() => {
+
+    async function loadScores() {
+
+      const response = await api.post('/pontuacaoCliente/resgatarPontosDoCliente', {
+        clienteId: idCliente,
+        quantidadePontosResgatados: pontosResgatados
+      });
+
+      totalPontosCliente = response.data.TotalPontosCliente;
+      mensagem = response.data.Mensagem;
+      
+      console.log(mensagem);
+    }
+
+    loadScores();
+  }, []);
+}
