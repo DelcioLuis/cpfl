9700d4e2fb8cea167548e204169244ac99eb7af7
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-15 21:24:03 -0300

    ajuste nome função

--- src/pages/Gamifica/ResgatePontos.js
+++ src/pages/Gamifica/ResgatePontos.js
@@ -5,14 +5,14 @@ export default function ResgatePontos() {
 
   const idCliente = "5fb05d60a3008ba9c6e59137"; // Delcio
 
-  const [pontosResgatados, setPontosResgatados] = useState(5);
+  const [pontosResgatados, setPontosResgatados] = useState(5); // trocar pelo valor resgatado na tela
 
   let totalPontosCliente = 0;
   let mensagem = "";
 
   useEffect(() => {
 
-    async function loadScores() {
+    async function EfetuarResgate() {
 
       const response = await api.post('/pontuacaoCliente/resgatarPontosDoCliente', {
         clienteId: idCliente,
@@ -27,6 +27,6 @@ export default function ResgatePontos() {
       return { totalPontosCliente, mensagem };
     }
 
-    loadScores();
+    EfetuarResgate();
   }, []);
 }
